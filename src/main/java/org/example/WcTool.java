b93f14622c4b3ed203c44b422847cb0576b265de
david.ferrari <david.ferrari@primeurad.com>
2024-09-05 21:48:38 +0200

    Step 4
    Added -m option to count all characters of the give file
    
    Step 4
    
    Added -m option to count all characters of the given file

--- src/main/java/org/example/WcTool.java
+++ src/main/java/org/example/WcTool.java
@@ -2,24 +2,30 @@ package org.example;
 
 import picocli.CommandLine;
 
+import javax.xml.stream.events.Characters;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.concurrent.Callable;
 
-@CommandLine.Command(name="ccwc", mixinStandardHelpOptions = true, version = "ccwc 1.0",
-description = "Counts the number of lines, words and characters")
+@CommandLine.Command(name = "ccwc", mixinStandardHelpOptions = true, version = "ccwc 1.0",
+        description = "Counts the number of lines, words and characters")
 public class WcTool implements Callable<Integer> {
 
-    @CommandLine.Option(names={"-c", "--count"}, description = "Count bytes in the file")
+    @CommandLine.Option(names = {"-c", "--count"}, description = "Count bytes in the file")
     private boolean countBytes;
 
-    @CommandLine.Option(names={"-l", "--line"}, description = "Count lines in the file")
+    @CommandLine.Option(names = {"-l", "--line"}, description = "Count lines in the file")
     private boolean countLines;
 
-    @CommandLine.Option(names={"-w", "--word"}, description = "Count words in the file")
+    @CommandLine.Option(names = {"-w", "--word"}, description = "Count words in the file")
     private boolean countWords;
 
-    @CommandLine.Parameters(index="0", description = "The file whose lines, words and characters are to be counted.")
+    @CommandLine.Option(names = {"-m", "--chars"}, description = "Count characters in the file")
+    private boolean countChars;
+
+    @CommandLine.Parameters(index = "0", description = "The file whose lines, words and characters are to be counted.")
     private String filePath;
 
     @Override
@@ -50,10 +56,29 @@ public class WcTool implements Callable<Integer> {
             outputBuilder.append(words);
         }
 
+        if (countChars) {
+            int chars = 0;
+            try (
+                    BufferedInputStream bis = new BufferedInputStream(Files.newInputStream(path));
+                    InputStreamReader isr = new InputStreamReader(bis, StandardCharsets.UTF_8);
+                    BufferedReader br = new BufferedReader(isr)
+            ) {
+                int codeUnit;
+                while ((codeUnit = br.read()) != -1) {
+                    chars++;
+                }
+            }
+
         outputBuilder.append("\t");
-        outputBuilder.append(path.getFileName().toString());
+        outputBuilder.append(chars);
+    }
+
+        outputBuilder.append("\t");
+        outputBuilder.append(path.getFileName().
+
+    toString());
         System.out.println(outputBuilder.toString());
 
         return 0;
-    }
+}
 }
